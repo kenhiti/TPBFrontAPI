c7c6eb750a6799352e30b8bf97dc05888750f310
FrancielleOliveira <franfm.oliveira@gmail.com>
2016-09-07 09:59:00 -0300

    alterações cadastro autor e sidebar

--- angularjs/controllers/cadastroAutorCtrl.js
+++ angularjs/controllers/cadastroAutorCtrl.js
@@ -1,13 +1,13 @@
 app.controller("cadastroAutorCtrl", function($scope,$routeParams, autorAPI) {
-	$scope.titulo = "Cadastrar Autor";
+	$scope.titulo = "Novo Autor";
 
 	$scope.autor = {};
 
 	$scope.message = "";
 	$scope.type = "";
-	$scope.render = false;		
-	
-	$scope.salvar = function(autor){		
+	$scope.render = false;
+
+	$scope.salvar = function(autor){
 
 		var $promisse;
 
@@ -15,7 +15,7 @@ app.controller("cadastroAutorCtrl", function($scope,$routeParams, autorAPI) {
 			$promisse = autorAPI.atualizarAutor(autor);
 		}
 		else{
-			$promisse = autorAPI.adicionarAutor(autor);	
+			$promisse = autorAPI.adicionarAutor(autor);
 		}
 
 		$promisse.then(function(success){
@@ -23,28 +23,28 @@ app.controller("cadastroAutorCtrl", function($scope,$routeParams, autorAPI) {
 			$scope.message = "Autor salvo com sucesso.";
 			$scope.type = "alert alert-success TexAlCenter";
 			$scope.render = true;
-			
+
 			$scope.titulo = "Cadastrar Autor";
 		}, function(error){
 			$scope.message = "Erro ao salvar dados.";
 			$scope.type = "alert alert-danger TexAlCenter";
 			$scope.render = true;
-		});		
+		});
 	};
 
 	$scope.close = function(){
 		$scope.render = !$scope.render;
-	}	
-	
+	}
+
 	var buscarEdicao = function(){
 		autorAPI.getAutorById($routeParams.id).success(function(data){
 			$scope.autor = data;
 		});
-	};	
-	
+	};
+
 	if($routeParams.id != undefined){
 		buscarEdicao();
 		$scope.titulo = "Editar autor";
 	}
-	
+
 });
